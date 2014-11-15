69414e05bc4610b749c36e63d0b7580ac19df137
JesseBuesking <jessebuesking@gmail.com>
2014-11-15 14:29:01 -0800

    fixing pypi build

--- setup.py
+++ setup.py
@@ -5,18 +5,22 @@ import os
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    # noinspection PyBroadException
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except:
+        return ""
 
 
 setup(
     name='clocked',
     packages=['clocked'],
-    version='0.1',
+    version='0.2',
     description='python library for high level profiling',
     author='Jesse Buesking',
     author_email='jessebuesking+pypi@gmail.com',
     url='https://github.com/JesseBuesking/clocked',
-    download_url='https://github.com/JesseBuesking/clocked/tarball/0.1',
+    download_url='https://github.com/JesseBuesking/clocked/tarball/0.2',
     keywords=['benchmark', 'benchmarking', 'profiler', 'profiling'],
     long_description=read('readme.md'),
     classifiers=[]
