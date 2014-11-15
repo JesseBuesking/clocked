0188ae94af695fb482aa8da8aadb5b0214430b4d
JesseBuesking <jessebuesking@gmail.com>
2014-11-15 14:00:25 -0800

    making a proper package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='clocked',
+    packages=['clocked'],
+    version='0.1',
+    description='python library for high level profiling',
+    author='Jesse Buesking',
+    author_email='jessebuesking+pypi@gmail.com',
+    url='https://github.com/JesseBuesking/clocked',
+    download_url='https://github.com/JesseBuesking/clocked/tarball/0.1',
+    keywords=['benchmark', 'benchmarking', 'profiler', 'profiling'],
+    classifiers=[],
+)
